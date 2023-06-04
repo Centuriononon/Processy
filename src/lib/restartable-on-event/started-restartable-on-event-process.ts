1e85dd0e5d02956e469de68dc3dd92fb8a3abc19
Centuriononon <centuriononon@gmail.com>
2023-06-04 18:33:43 +0400

    [fix] fixed unstopping restartable-on-event processes on stop() method call

--- src/lib/restartable-on-event/started-restartable-on-event-process.ts
+++ src/lib/restartable-on-event/started-restartable-on-event-process.ts
@@ -7,7 +7,7 @@ export class StartedRestartableOnEventProcess<State, Msg>
 	implements IStartedRestartableProcess<State, Msg>
 {
 	protected count: number = 0;
-	protected __working: boolean = false;
+	protected stopped: boolean = false;
 
 	constructor(
 		protected readonly process: IStartedRestartableProcess<State, Msg>,
@@ -22,10 +22,8 @@ export class StartedRestartableOnEventProcess<State, Msg>
 	working = this.process.working.bind(this.process);
 	message = this.process.message.bind(this.process);
 	stop(status: 'OK' | 'BAD') {
-		this.__working = false;
-		this.process.stop(status);
-
-		return OK;
+		this.stopped = true;
+		return this.process.stop(status);
 	}
 	restart = this.process.restart.bind(this.process);
 
@@ -38,16 +36,16 @@ export class StartedRestartableOnEventProcess<State, Msg>
 
 		this.process
 			.sub(this.args.event, value => {
-				const isLoop = this.args.times === Infinity;
-				const isLast = ++this.count === this.args.times;
-
-				isLoop || !isLast && !this.__working
-					? this.restart('OK')
-					: this.pub(this.args.event, value);
+				if (!this.stopped) {
+					const isLoop = this.args.times === Infinity;
+					const isLast = ++this.count === this.args.times;
+	
+					isLoop || !isLast
+						? this.restart('OK')
+						: this.pub(this.args.event, value);
+				}
 			})
 			.init(state);
-
-		this.__working = true;
 
 		return this;
 	}
